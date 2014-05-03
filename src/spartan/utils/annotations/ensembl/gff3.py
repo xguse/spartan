7230567c7527f6a3543c246cd1429d70e481cf4a
Augustine Dunn <wadunn83@gmail.com>
2014-05-03 13:12:28 -0700

    created files: - utils/annotations/ensembl/gff3.py - utils/annotations/intervals.py

---
+++ src/spartan/utils/annotations/ensembl/gff3.py
@@ -0,0 +1,14 @@
+# gff3 is part of the 'spartan' package.
+# It was written by Gus Dunn and was created on 5/3/14.
+# 
+# Please see the license info in the root folder of this package.
+
+"""
+=================================================
+gff3
+=================================================
+Purpose:
+
+"""
+__author__ = 'Gus Dunn'
+
---
+++ src/spartan/utils/annotations/intervals.py
@@ -0,0 +1,14 @@
+# intervals.py is part of the 'spartan' package.
+# It was written by Gus Dunn and was created on 5/3/14.
+# 
+# Please see the license info in the root folder of this package.
+
+"""
+=================================================
+intervals.py
+=================================================
+Purpose:
+
+"""
+__author__ = 'Gus Dunn'
+
