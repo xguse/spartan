d219feb6d2d309f1c1bf87ed85cdc1fa1240809a
Augustine Dunn <wadunn83@gmail.com>
2014-05-03 13:14:47 -0700

    src/spartan/utils/annotations/intervals.py: - added class CompoundFeature - added class SimpleFeature - docstring outlined SimpleFeature

--- src/spartan/utils/annotations/intervals.py
+++ src/spartan/utils/annotations/intervals.py
@@ -8,7 +8,104 @@
 intervals.py
 =================================================
 Purpose:
+This code is intended to provide the "base" interval representation for `spartan`.
 
 """
 __author__ = 'Gus Dunn'
 
+
+#### Helper functions ####
+def interval_length(start, end):
+    return end - start + 1
+
+
+##########################
+
+class CompoundFeature(object):
+    def __init__(self):
+        pass
+
+
+
+
+class SimpleFeature(object):
+
+    def __init__(self, start, end, seqid=None):
+
+        """
+
+
+        :param start: left most coordinate.
+        :param end: right most coordinate.
+        :param seqid: ID of the sequence that this interval is annotated on.
+        :param name:
+        """
+
+        assert isinstance(start, int)
+        assert isinstance(end, int)
+        self.start = min([start, end])
+        self.end = max([start, end])
+        self.seqid = seqid
+
+    def __len__(self):
+        return interval_length(self.start, self.end)
+
+    def __contains__(self, item):
+        pass
+
+    def __cmp__(self, other):
+        """
+        :param other: an interval/feature
+        :returns int:
+            * `-1` if `other` should sort to the right of `self`
+            * `0` if `other` should sort exactly the same as `self`
+            * `1` if `other` should sort to the left of `self`
+
+        """
+        pass
+
+    def __eq__(self, other):
+        """
+        Returns `True` if `other` perfectly overlaps this feature, `False` otherwise.
+        :param other: an interval/feature
+        """
+        pass
+
+    def __gt__(self, other):
+        """
+        Returns `True` if `other` falls to the right and does not overlap this feature, `False` otherwise.
+        :param other: an interval/feature
+        """
+        pass
+
+    def __ge__(self, other):
+        """
+        Returns `True` if `other` falls to the right even if `other` overlaps this feature,
+        `False` otherwise.
+        :param other: an interval/feature
+        """
+        pass
+
+    def __lt__(self, other):
+        """
+        Returns `True` if `other` falls to the left and does not overlap this feature, `False` otherwise.
+        :param other: an interval/feature
+        """
+        pass
+
+    def __le__(self, other):
+        """
+        Returns `True` if `other` falls to the left even if `other` overlaps this feature,
+        `False` otherwise.
+        :param other: an interval/feature
+        """
+        pass
+
+    def flank(self, length):
+        pass
+
+    def rflank(self, length):
+        pass
+
+    def lflank(self, length):
+        pass
