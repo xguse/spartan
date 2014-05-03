f048f2c6375d4a663252387c37448f974708281c
Augustine Dunn <wadunn83@gmail.com>
2014-05-03 13:16:48 -0700

    src/spartan/utils/annotations/ensembl/gff3.py - added SimpleFeatureGFF3(intervals.SimpleFeature)

--- src/spartan/utils/annotations/ensembl/gff3.py
+++ src/spartan/utils/annotations/ensembl/gff3.py
@@ -8,7 +8,36 @@
 gff3
 =================================================
 Purpose:
-
+Handle parsing and storage of gff3 data
 """
+from spartan.utils.annotations import intervals
+
 __author__ = 'Gus Dunn'
 
+
+class SimpleFeatureGFF3(intervals.SimpleFeature):
+
+    def __init__(self, start, end, seqid=None, source=None, type=None,
+                 score=None, strand=None, phase=None, attributes=None):
+
+        """
+        Initializes `SimpleFeatureGFF3` object with the fields from a single GFF3 line.
+
+        :param start:
+        :param end:
+        :param seqid:
+        :param source:
+        :param type:
+        :param score:
+        :param strand:
+        :param phase:
+        :param attributes:
+        """
+        pass
+
+    def _parse_attributes(self, attributes):
+        """
+        Returns a `dict`-like object from the key/value data encoded in the attributes string.
+
+        :param attributes: attributes string from a GFF3 line.
+        """
